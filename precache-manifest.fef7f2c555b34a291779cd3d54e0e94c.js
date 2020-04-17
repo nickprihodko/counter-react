0504d7b02c7043a4bd3c1284875d28233b1f6010
Prihodko Nick <prihodkonn@gmail.com>
2020-04-17 17:07:30 +0300

    Updates

---
+++ precache-manifest.fef7f2c555b34a291779cd3d54e0e94c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12463a1040c897704c6d436878d767b1",
+    "url": "/counter-react/index.html"
+  },
+  {
+    "revision": "3cbc8fc6ffdde43e8aef",
+    "url": "/counter-react/static/css/main.bbdd5259.chunk.css"
+  },
+  {
+    "revision": "69e102158006821a53cc",
+    "url": "/counter-react/static/js/2.c04473b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter-react/static/js/2.c04473b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cbc8fc6ffdde43e8aef",
+    "url": "/counter-react/static/js/main.01bb3680.chunk.js"
+  },
+  {
+    "revision": "54598bf148d6dfe35c69",
+    "url": "/counter-react/static/js/runtime-main.232a9e65.js"
+  }
+]);
